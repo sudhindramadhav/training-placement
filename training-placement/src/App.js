19e4dc0e8deda1f6cb0d56e053995225660c1e7d
sudhindramadhav <k.k.m.sudhindra@hotmail.com>
2024-04-28 11:51:56 +0530

    commit message final

---
+++ training-placement/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import About from './component/About'
+import PlacementIncharge from './component/Placement_Incharge';
+import Alumini from './component/Alumini';
+import StudentLogin from './component/student/StudentLogin';
+import StudentWorkshop from './component/student/StudentWorkshop';
+import StudentInternship from './component/student/StudentInternship';
+import StudentPlacementDrive from './component/student/StudentPlacementDrive';
+
+function App() {
+  return (
+    <>
+      <Router scrollRestoration="auto">
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path = '/about' element={<About />}/>
+          <Route path = '/placement_incharge' element={<PlacementIncharge />}/>
+          <Route path = '/alumini' element={<Alumini />}/>
+          <Route path="/home" element={<Home />} />
+    
+          <Route path="/student_login" element={<StudentLogin/>} />
+          <Route path="/studentworkshop" element={<StudentWorkshop/>} />
+          <Route path = "/studentinternship" element={<StudentInternship/>}/>
+          <Route path = "/studentplacementdrive" element={<StudentPlacementDrive/>}/>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ training-placement/src/component/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div>
+      <header className="text-gray-1000 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <Link to="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl">Placement Cell</span>
+          </Link>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <Link to="/home" className="mr-5 hover:text-gray-900">Home</Link>
+            <Link to="/about" className="mr-5 hover:text-gray-900">About</Link>
+            <Link to="/placement_incharge" className="mr-5 hover:text-gray-900">Placement Incharge</Link>
+            <Link to="/alumini" className="mr-5 hover:text-gray-900">Alumni</Link>
+            <Link to="/student_login" className="mr-5 hover:text-gray-900">StudentLogin</Link>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+}
